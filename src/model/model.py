48cb1a8e7489041478e29b5e0ad483da832788c4
Tanmay Sharma <tanmays369@gmail.com>
2024-02-01 20:07:58 +0530

    Update model.py

--- src/model/model.py
+++ src/model/model.py
@@ -1,53 +1,9 @@
-from torch import nn
+import torch.nn as nn
 from layers import ConvBlock, MiddleBlock, ExitBlock
 
-class Xception56(nn.Sequential):
-    def __init__(self, config, classify=True):
-        super(Xception56, self).__init__()
-
-        in_ch= config["in_ch"]
-        channels= config["channels"]
-        strides= config["strides"]
-        n_classes= config["n_classes"]
-
-        self.add_module('entry_flow', ConvBlock(in_ch, channels[0], k=3, s=2, pad=1))
-
-        middle_blocks = [
-            MiddleBlock(in_ch=channels[i-1], out_ch=channels[i], stride=strides[i-1])
-            for i in range(1, len(channels))
-        ]
-        self.add_module('middle_flow', nn.Sequential(*middle_blocks))
-
-        self.add_module('exit_flow', ExitBlock(channels[-2], channels[-1], strides[-1]))
-
-        if classify:
-            self.add_module(
-                name='fc',
-                module=nn.Sequential(
-                    nn.AdaptiveAvgPool2d(1),
-                    nn.Flatten(),
-                    nn.Linear(channels[-1], n_classes)
-                )
-            )
-
-    def forward(self, inputs):
-        return super(Xception56, self).__init__(inputs)
-
-config = {
-    "in_ch":3,
-    "n_classes":10,
-    "channels":[32,64,128,256,728],
-    "strides":[1,2,1,2]
-}
-
-model= Xception56(config, classify=True)
-print(model(torch.randn(1,3,224,224)).shape)
-
-##########Xception71
-
-class Xception71(nn.Sequential):
-    def __init__(self, config, classify=True):
-        super(Xception71, self).__init__()
+class Xception(nn.Sequential):
+    def __init__(self, version, config, classify=True):
+        super(Xception, self).__init__()
 
         in_ch = config["in_ch"]
         channels = config["channels"]
@@ -75,14 +31,4 @@ class Xception71(nn.Sequential):
             )
 
     def forward(self, inputs):
-        return super(Xception71, self).__init__(inputs)
-
-config = {
-    "in_ch": 3,
-    "n_classes": 10,
-    "channels": [32, 64, 128, 256, 728, 728, 728],
-    "strides": [1, 2, 1, 2, 1, 2, 1]
-}
-
-model = Xception71(config, classify=True)
-print(model(torch.randn(1, 3, 224, 224)).shape)
+        return super(Xception, self).forward(inputs)
